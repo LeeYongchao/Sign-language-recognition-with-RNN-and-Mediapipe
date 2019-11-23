162e98d310e44173de0c2a65e59c160a20f3913a
KJongUk <41438904+KJongUk@users.noreply.github.com>
2019-11-23 21:45:34 +0900

    Update make_pickle.py

--- util/make_pickle.py
+++ util/make_pickle.py
@@ -4,43 +4,40 @@ import os
 import sys
 import argparse
 import random
-
 def search(dirname):
-    listfile=os.listdir(dirname)
-    predict=[]
-    maxlen=0
-    listlength=[]
-    for file in listfile:
-        wordname=file
-        textlist=os.listdir(dirname+wordname)
-        #print(wordname)
-        for namet in textlist:
-            textnamed=dirname+wordname+"/"+namet
-            with open(textnamed) as datad:
-                lend=[[i for i in line.split(' ')][:-1] for line in datad.readlines()]
-                listlength.append(len(lend[0]))
-                #print(len(lend))
-                #print(len(lend[0]))
-        maxlen=max(listlength)
-    #print("최대길이:")
-    #print(maxlen)
-    for file in listfile:
-        wordname=file
-        textlist=os.listdir(dirname+wordname)
-        for text in textlist:
-            if "_" in text:
-                continue;		
-            textname=dirname+wordname+"/"+text
-            with open(textname) as data:
-                numbers = [[i for i in line.split(' ')][:-1] for line in data.readlines()]
-                #print(len(numbers[0]))
-                for i in range(len(numbers[0]),maxlen):
-                    numbers[0].extend([0.000])
-                numbers.append(wordname)
+	listfile=os.listdir(dirname)
+	predict=[]
+	maxlen=0
+	listlength=[]
+	for file in listfile:
+		if "_" in file:
+			continue
+		wordname=file
+		textlist=os.listdir(dirname+wordname)
+		for namet in textlist:
+			textnamed=dirname+wordname+"/"+namet
+			with open(textnamed) as datad:
+				lend=[[i for i in line.split(' ')][:-1] for line in datad.readlines()]
+				listlength.append(len(lend[0]))
+		maxlen=max(listlength)
+	for file in listfile:
+		if "_" in file:
+			continue
+		wordname=file
+		textlist=os.listdir(dirname+wordname)
+		for text in textlist:
+			textname=dirname+wordname+"/"+text
+			with open(textname) as data:
+				numbers = [[i for i in line.split(' ')][:-1] for line in data.readlines()]
+        	        #print(len(numbers[0]))
+				for i in range(len(numbers[0]),maxlen):
+					numbers[0].extend([0.000])
+				numbers.append(wordname)
                 #print(numbers[0][8735])
-            predict.append(numbers)
-    random.shuffle(predict)        
-    return predict
+			predict.append(numbers)
+	random.shuffle(predict)        
+	return predict
+
 
 def main(inputfile_path,output_path):
 	ret=search(inputfile_path)
