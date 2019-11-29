a0fc6566d8a72da337cb91eb45ade5ea2d2cd2bb
KJongUk <41438904+KJongUk@users.noreply.github.com>
2019-11-29 14:19:27 +0900

    Update predict.py

--- model/predict.py
+++ model/predict.py
@@ -1,4 +1,14 @@
 
+
+def load_labels(label_file):
+    label = {}
+    count = 0
+    proto_as_ascii_lines = tf.gfile.GFile(label_file).readlines()
+    for l in proto_as_ascii_lines:
+        label[l.strip()] = count
+        count += 1
+    return label
+
 x_test,y_test=load_data("/Users/anna/SLR/Seperate/testinput/")
 new_model = tf.keras.models.load_model('simpeRNN.h5')
 new_model.summary()
@@ -15,7 +25,7 @@ new_model.summary()
 xhat = x_test
 yhat = new_model.predict(xhat)
 print('## yhat ##')
-
+#labels=load_label(dirname) 
 a=xhat.shape[0]
 for i in range(a):
     print('True: '+str(argmax(y_test[i]))+', Predict: '+str(yhat[i]))
